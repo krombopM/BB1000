7e1caed1e9f169ed4d0b0efba5e94489aaea5c62
Emil Spåre <Montop@gmail.com>
2018-05-04 15:31:10 +0200

    finished

--- ExtLibraries/test.py
+++ ExtLibraries/test.py
@@ -1,15 +1,13 @@
-year = list(yearBirths.year)
-births = list(yearBirths.births)
-listOfIndices = []
-years = list(range(1880, 2010))
-for item in years:
-    listOfIndices.append(year.count(item))
-listOfIndices.insert(0,0)
-actualListOfIndices = []
-for item in range(len(listOfIndices)):
-    actualListOfIndices.append(item + sum(listOfIndices[0:item]))
-del actualListOfIndices[1]
-print(actualListOfIndices)
-sumOfBirthsPerYear = []
-for item in actualListOfIndices[1:-1]:
-    amount = sum
+sp.run('cat 3IE9.pdb | grep "^HETATM" > hetatm.csv', shell=True)
+linesToBeWritten = []
+with open("hetatm.csv", "r") as f:
+    for line in f:
+        linesToBeWritten.append(" ".join(line.split()).replace(" ", ",") + "\n")
+linesToBeWritten.insert(0, "hetatm,a,b,c,d,e,x,y,z,f,g,h\n")
+with open("hetatm.csv", "w") as f:
+    for item in linesToBeWritten:
+        f.write(item)
+df_other = pd.read_csv("out_top1000.csv")
+hetatmX = df_other["x"].values.tolist()
+hetatmY = df_other["y"].values.tolist()
+hetatmZ = df_other["z"].values.tolist()
